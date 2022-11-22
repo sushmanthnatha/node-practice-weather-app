2ff871c5d3e618a6305b79a44a4dc0bf84618c5b
sushmanth <sushmanthnatha@gmail.com>
2022-11-23 00:30:35 +0530

    simply using both weather forecast and geocoding api calls

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-
-// weather-stack key 7cbba7d0dacc63824c6b0d2dc2f0cc7a
-
-
 const request = require('postman-request')
 
-const url = 'http://api.weatherstack.com/current?access_key=7cbba7d0dacc63824c6b0d2dc2f0cc7a&query=Hyderabad'
 
+const weather_stack_access_key = '7cbba7d0dacc63824c6b0d2dc2f0cc7a'
+const position_stack_access_key = '587687d5b67c7827d315e836abd107c4'
+
+const url = 'http://api.weatherstack.com/current?access_key='+ weather_stack_access_key +'&query=Hyderabad&units=f'
+const url2 = 'http://api.positionstack.com/v1/forward?access_key='+ position_stack_access_key +'&query=1600 Pennsylvania Ave NW, Washington DC&limit=1'
 request( { url : url, json : true}, (error, response) => {
 
-    const data = JSON.parse(response.body);
-    // console.log(data.current)
+    const data = response.body;
+    
+    console.log('In ',data.request.query,' it is currently ',data.current.temperature,' deg temperature and its feeling like ',data.current.feelslike)
 
-    console.log(response.body.current)
 })
 
+
+request( { url : url2, json : true}, (error, response) => {
+    const data2 = response.body.data[0]
+    //console.log(data2)
+    console.log('Place : ',data2.label,'\n Lat : ',data2.latitude,', Long: ',data2.longitude)
+})
   
